eaade592ef3c889b4e023f9b4dbeb49329f3df8e
Clarefollett <chlaw101@gmail.com>
2017-02-17 10:59:51 -0800

    tests passing for signin, requires username and password and compares to see if they match, also uses hash for password

--- test/user.test.js
+++ test/user.test.js
@@ -11,4 +11,20 @@ describe('user model', () => {
     it('requires a hash for password', () => {
         return testInvalid({ username: 'username' });
     });
+
+    it('is valid w username and password', () => {
+        return new User({ username: 'username', password: 'hi'}).validate();
+    });
+
+    it('uses hash for password correctly and compares', () => {
+        const data = { username: 'username', password: 'hi'};
+        const user = new User(data);
+
+        assert.isUndefined(user.password);
+        assert.notEqual(user.hash, data.password);
+
+        assert.isTrue(user.comparePassword('hi'));
+        assert.isFalse(user.comparePassword('incorrect password'));
+    });
+
 });
