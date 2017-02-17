fa5f5c06aebb34eb07f9cbd485567082a66f31c5
Clarefollett <chlaw101@gmail.com>
2017-02-17 11:17:16 -0800

    error handler test passing

---
+++ test/error-handler.test.js
@@ -0,0 +1,17 @@
+const errorHandler = require('../lib/error-handling')();
+const assert = require('chai').assert;
+
+describe('error handler', () => {
+
+    it('returns 500 when no code provided', () => {
+        const err = new Error('error');
+        const res = {
+            status(code) { this.code = code; return this; },
+            send(error) { this.error = error; }
+        };
+
+        errorHandler(err, null, res, null);
+        assert.equal(res.code, 500);
+        assert.deepEqual(res.error.error, 'Internal Server Error');
+    });
+});
